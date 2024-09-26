df3916cf104fb0c6f7247cd69c10ba2d6edda123
David Benjamin <davidnbenjamin15@gmail.com>
2024-09-26 17:15:53 -0400

    Add register page and link to register page on sign in.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
-import { LoginForm } from './components/login-form';
+import { LoginForm } from './components/LoginForm';
 import Navbar from './components/navbar/Navbar';
+import Link from 'next/link';
 
 export default function Home() {
     return (
         <body>
             <Navbar />
             <div className='w-full flex justify-center'>
-                <div className='bg-gray-100 min-w-100 w-1/2 px-10 my-10 shadow-md rounded-lg'>
+                <div className='bg-gray-100 w-1/2 max-w-lg px-10 my-10 shadow-md rounded-lg'>
                     <div className='flex justify-center'>
                         <h1>Sign in</h1>
                     </div>
 
                     <main className='flex justify-center w-full'>
                         <LoginForm />
+                        
                     </main>
+                    <div>
+                        <p className="text-sm flex justify-center mb-5">Don't have an account?<Link href='/register-account' className='text-blue-600 ml-3'>Register</Link></p>
+                        
+                    </div>
+
                 </div>
             </div>
 
