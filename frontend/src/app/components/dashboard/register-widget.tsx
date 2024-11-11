f5eedb93428d74b59a83857d90528755cb0dbd2d
David Benjamin <davidnbenjamin15@gmail.com>
2024-11-11 17:34:32 -0500

    Add registration form if widget developer has no widgets

---
+++ frontend/src/app/components/dashboard/register-widget.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import React from 'react'
+import { Card, CardContent, CardHeader } from '../ui/card'
+import { Button } from '../ui/button'
+import { useRouter } from 'next/navigation';
+
+export default function RegisterWidget() {
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push('/register-widget');
+    }
+
+  return (
+    <Card>
+        <CardHeader>Don't see any widgets?</CardHeader>
+        <CardContent className='flex justify-center'>
+            <Button onClick={handleClick}>
+                Register a widget
+            </Button>
+        </CardContent>
+    </Card>
+  )
+}
--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -4,15 +4,22 @@ import Navbar from "../components/navbar/Navbar";
 import Widgets from "../components/dashboard/widgets";
 import Search from "../components/dashboard/search";
 import ProtectedRoute from "../components/ProtectedRoutes";
+import { $widgets } from "@/lib/store";
+import { useStore } from "@nanostores/react";
+import { useEffect } from "react";
+import RegisterWidget from "../components/dashboard/register-widget";
+import useQueryWidgets from "../hooks/use-query-widgets";
 
 export default function Dashboard() {
+  const { widgets } = useQueryWidgets();
 
   return (
     <ProtectedRoute>
       {/* <Search /> */}
       <Navbar />
-      <div className="flex justify-center">
-        <Widgets />
+      <div className="flex justify-center p-10">
+        {widgets.length > 0 ? <Widgets /> : <RegisterWidget />}
+
       </div>
     </ProtectedRoute>
   );
