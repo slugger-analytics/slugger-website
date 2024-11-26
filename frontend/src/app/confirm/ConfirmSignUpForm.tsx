ecefa3bab91ee103ba8d9581cc834165cc81e8ba
David Benjamin <davidnbenjamin15@gmail.com>
2024-11-26 16:36:48 -0500

    Restyle confirmation form

--- frontend/src/app/confirm/ConfirmSignUpForm.tsx
+++ frontend/src/app/confirm/ConfirmSignUpForm.tsx
@@ -4,6 +4,14 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { confirmSignUp } from "../../services/authService"; // You'll implement this function
 import SubmitButton from "../components/input/SubmitButton";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/app/components/ui/card"
+import { Input } from "@/app/components/ui/input"
 
 // Component for confirming sign-up
 export function ConfirmSignUpForm() {
@@ -20,13 +28,18 @@ export function ConfirmSignUpForm() {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
+    setSubmitStatus({
+      message: "",
+      textClass: "text-black",
+    });
+
     try {
       // Call the confirmSignUp function to verify the account
       await confirmSignUp(email, confirmationCode);
       // Update submission status on success
       setSubmitStatus({
         message: "Account confirmed! Redirecting to login...",
-        textClass: "text-green-500",
+        textClass: "text-green-600",
       });
       // Redirect to login after successful confirmation
       setTimeout(() => router.push("/sign-in"), 200);
@@ -35,41 +48,49 @@ export function ConfirmSignUpForm() {
       setSubmitStatus({
         message:
           error instanceof Error ? error.message : "Confirmation failed.",
-        textClass: "text-red-500",
+        textClass: "text-red-600",
       });
     }
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col items-center w-80">
-      {/* Input field for email */}
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Enter your email"
-        className="input-field"
-        required
-      />
-      {/* Input field for confirmation code */}
-      <input
-        type="text"
-        value={confirmationCode}
-        onChange={(e) => setConfirmationCode(e.target.value)}
-        placeholder="Enter confirmation code"
-        className="input-field"
-        required
-      />
-      {/* Submit button */}
-      <SubmitButton btnText="Confirm Sign Up" />
-      {/* Display submission status message */}
-      <p
-        aria-live="polite"
-        className={`mb-2 ${submitStatus.textClass}`}
-        role="status"
-      >
-        {submitStatus.message}
-      </p>
-    </form>
+    <Card>
+      <CardHeader className="flex items-center justify-center">
+        <CardTitle>You&apos;re almost there</CardTitle>
+        <CardDescription>Check your email for a confirmation code</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit} className="flex flex-col items-center w-80">
+          <div className="grid w-full items-center gap-4">
+            {/* Input field for email */}
+            <Input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email"
+              className="input-field"
+              required
+            />
+            {/* Input field for confirmation code */}
+            <Input
+              type="text"
+              value={confirmationCode}
+              onChange={(e) => setConfirmationCode(e.target.value)}
+              placeholder="Enter confirmation code"
+              className="input-field"
+              required
+            />
+            {/* Submit button */}
+            <SubmitButton btnText="Confirm Sign Up" />
+            {/* Display submission status message */}
+            <div className="flex justify-center text-sm">
+              <p className={`my-2 ${submitStatus.textClass}`} role="status" aria-live="polite">{submitStatus.message}</p>
+            </div>
+          </div>
+        </form>
+
+      </CardContent>
+    </Card>
+
   );
 }
--- frontend/src/app/confirm/page.tsx
+++ frontend/src/app/confirm/page.tsx
@@ -2,28 +2,15 @@
 import { ConfirmSignUpForm } from "./ConfirmSignUpForm";
 import Navbar from "../components/navbar/Navbar";
 
+
 // Define the Home component as the default export
 export default function Home() {
   return (
-    // Define the body of the HTML document
     <div>
-      {/* Render the Navbar component */}
       <Navbar />
-      {/* Center the content horizontally */}
-      <div className="w-full flex justify-center">
-        {/* Define a container with specific styling */}
-        <div className="bg-gray-100 w-1/2 max-w-lg px-10 my-10 shadow-md rounded-lg">
-          {/* Center the heading horizontally */}
-          <div className="flex justify-center">
-            <h1 className="text-center">Sign up for an account</h1>
-          </div>
-          {/* Main content area */}
-          <main className="flex justify-center w-full">
-            {/* Render the ConfirmSignUpForm component */}
-            <ConfirmSignUpForm />
-          </main>
-        </div>
+      <div className="flex flex-col items-center justify-center p-20">
+        <ConfirmSignUpForm />
       </div>
     </div>
-  );
+  )
 }
