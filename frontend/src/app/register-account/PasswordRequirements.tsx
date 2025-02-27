4932f8e89c7b96ff2b2e7317dcd28d83210a9dee
BryanSantana <bryansantana0210@gmail.com>
2025-02-27 01:38:28 -0500

    Type error issue

--- frontend/src/app/register-account/PasswordRequirements.tsx
+++ frontend/src/app/register-account/PasswordRequirements.tsx
@@ -1,14 +1,14 @@
 export default function PasswordRequirements() {
   return (
-    <p className="text-xs text-gray-500">
-      Password must contain:
+    <div className="text-xs text-gray-500">
+      <span>Password must contain:</span>
       <ul className="list-disc list-inside mt-1">
         <li>At least 8 characters</li>
         <li>One uppercase letter</li>
         <li>One lowercase letter</li>
         <li>One number</li>
-        <li>One special character (!@#$%^&*(),.?":{}|{'<>'})</li>
+        <li>One special character (!@#$%^&*(),.?&quot;:{}|{'<>'})</li>
       </ul>
-    </p>
+    </div>
   );
 } 
