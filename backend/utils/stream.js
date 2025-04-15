9ee140b1cf1bf11158aaab7bf07cb87c2cc0eddc
David <davidnbenjamin15@gmail.com>
2025-04-15 12:25:13 -0400

    Add endpoint to fetch league standings

---
+++ backend/utils/stream.js
@@ -0,0 +1,8 @@
+export async function streamToString(stream) {
+    return new Promise((resolve, reject) => {
+        const chunks = [];
+        stream.on("data", (chunk) => chunks.push(chunk));
+        stream.on("error", reject);
+        stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
+    });
+}
